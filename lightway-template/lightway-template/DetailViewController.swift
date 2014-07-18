f47f40aa9ed3cd512d20d766832497753d393ee9
iida <hayato.iida@speee.jp>
2014-07-18 15:23:46 +0900

    init

---
+++ lightway-template/lightway-template/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  lightway-template
+//
+//  Created by hayato.iida on 2014/07/17.
+//  Copyright (c) 2014年 hayato.iida. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var detailDescriptionLabel: UILabel
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lightway-template/lightway-templateTests/lightway_templateTests.swift
@@ -0,0 +1,36 @@
+//
+//  lightway_templateTests.swift
+//  lightway-templateTests
+//
+//  Created by hayato.iida on 2014/07/17.
+//  Copyright (c) 2014年 hayato.iida. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class lightway_templateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
